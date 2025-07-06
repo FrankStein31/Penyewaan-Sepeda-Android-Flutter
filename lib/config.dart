9acd09b773f45b2125dd26d198df768f9e31a185
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-07-06 21:18:41 +0700

    i[

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String baseUrl = 'http://192.168.0.56:3000/api';
   static const String apiUrl = baseUrl;
 }
